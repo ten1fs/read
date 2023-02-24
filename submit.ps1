aaa516fe2c90cfa0e89e0aa954d3cbb8058026ba
tenifs <ten1fs@outlook.com>
2023-02-24 10:38:48 +0800

    😃: 2023-02-24 10:38:48

--- submit.ps1
+++ submit.ps1
@@ -1,7 +1,1 @@
-echo "---- 开始执行 ----"
-$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
-$emoji_list = @('😀', '😁', '😃', '😎', '😊', '😘', '🤔')
-git add .
-git commit -m "$($emoji_list | Get-Random): $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
-git push
-echo "---- 任务完成 ----"
+cat ./script.ps1 -encoding utf8 | Invoke-Expression
